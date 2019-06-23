60c320099ae3f711b2f6d9862a9849452752b555
Atharva-Kapare <51484135+Atharva-Kapare@users.noreply.github.com>
2019-06-24 01:16:46 +1000

    Added a population, obstacle and a collision sys
    The collision system needs work.
    Also need to create a random array with the "genes" so that they travel on random paths.
    Need to add a start and end point, will try to have them as mouse presses but might hardcode them for now.
    Need to add evolution.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,80 @@
+let bugs = [];
+let size = 10;
 
-function setup(){
+function setup() {
 
     createCanvas(600, 600);
-    
-    let population = 10;
-    let i = 0;
-    for(i; i < population; i++){
-        bug = new Bug;
+    for (let i = 0; i < size; i++) {
+        bugs[i] = new Bug;
     }
 
 }
 
-function draw(){
+function draw() {
     background(0);
-    
-    //ellipse(300, 300, 50, 50);
-    //noFill;
-    bug.show();
-    
+
+    //Drawing all Bugs
+    for (var i = 0; i < size; i++) {
+        bugs[i].show();
+        bugs[i].move();
+
+        if (bugs[i].x > 620) {
+            bugs[i].x = -20;
+        }
+        if (bugs[i].y > 620) {
+            bugs[i].y = -20;
+        }
+
+        //Collision system, still needs more work. 
+        if (bugs[i].x > 112.5 || bugs[i] < 487.5) {
+            if (bugs[i].y > 285 && bugs[i].y < 315) {
+                bugs.splice(i, 1);
+                size -= 1;
+            }
+        }
+    }
+
+    //Drawing static obsticle
+    rectMode(CENTER);
+    rect(300, 300, 375, 30);
+
+    // for (var i = 0; i < size; i++) {
+    //     if (bugs[i].x > 112.5 && bugs[i] < 487.5) {
+    //         if (bugs[i].y > 285 && bugs[i].y < 315) {
+    //             bugs.splice(i, 1);
+    //             size--;
+    //         }
+    //     }
+    // }
+
+
 
 }
 
-class Bug{
-    constructor(){
-        this.x = Math.floor(Math.random()*300);
-        this.y = Math.floor(Math.random()*300);        
+class Bug {
+    constructor() {
+        this.x = Math.floor(Math.random() * 600);
+        this.y = Math.floor(Math.random() * 600);
     }
 
-    show(){
-        ellipse(this.x , this.y, 50,50);
+    show() {
+        ellipse(this.x, this.y, 10, 10);
         noFill;
     }
 
-    move(){
-
+    move() {
+        this.x += Math.random() * 10;
+        this.y += Math.random() * 10;
     }
 }
+
+function mousePressed() {
+    noLoop();
+}
+
+
+
+
+
 
 
