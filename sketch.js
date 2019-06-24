baed962501bf2b55072ea886f503a4f896575f25
Atharva-Kapare <51484135+Atharva-Kapare@users.noreply.github.com>
2019-06-24 10:34:49 +1000

    created the starting and ending point
    The bugs now die if they exit the border as well as when they hit the obstacle.
    The bugs also have a starting and ending point, but I am yet to give them the "DNA".

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 let bugs = [];
-let size = 10; //the size of the population for each generation
+let size = 100; //the size of the population for each generation
 let currentPopulation = size; //amount of bugs alive at any given time
 let speed = 5; //the speed at which the bugs can move
-
+let startX = 300;
+let startY = 450;
+let endX = 300;
+let endY = 150;
 
 function setup() {
 
@@ -17,22 +20,32 @@ function setup() {
 function draw() {
     background(0);
 
+    fill(0, 255, 0);
+    ellipse(startX, startY, 5, 5); //create the starting point
+
+    fill(255, 0, 0);
+    ellipse(endX, endY, 5, 5); //create the ending point
+
+    fill(0, 0, 0);
+
+
     //Drawing all Bugs
 
     for (var i = 0; i < currentPopulation; i++) {
         bugs[i].show();
         bugs[i].move();
 
-        if (bugs[i].x > 620) {
-            bugs[i].x = -20;
+        if (bugs[i].x > 600 || bugs[i].x < 0) {
+            bugs.splice(i, 1);
+            currentPopulation -= 1;
         }
-        if (bugs[i].y > 620) {
-            bugs[i].y = -20;
+        if (bugs[i].y > 600 || bugs[i].y < 0) {
+            bugs.splice(i, 1);
+            currentPopulation -= 1;
         }
 
-        hit = collideRectCircle(112, 285, 374, 30, bugs[i].x, bugs[i].y, 10)
-
-        //Collision system, still needs more work. 
+        //Collision system 
+        hit = collideRectCircle(112, 285, 374, 30, bugs[i].x, bugs[i].y, 5);
         if (hit) {
             bugs.splice(i, 1);
             currentPopulation -= 1;
@@ -48,18 +61,21 @@ function draw() {
 
 class Bug {
     constructor() {
-        this.x = Math.floor(Math.random() * 600);
-        this.y = Math.floor(Math.random() * 600);
+        this.x = startX;
+        this.y = startY;
+
+        // this.x = Math.floor(Math.random() * 600);
+        // this.y = Math.floor(Math.random() * 600);
     }
 
     show() {
         ellipse(this.x, this.y, 10, 10);
-        noFill;
+        fill(255, 255, 255);
     }
 
     move() {
-        this.x += ((Math.random() * 2) -1) * speed;
-        this.y += ((Math.random() * 2) -1) * speed;
+        this.x += ((Math.random() * 2) - 1) * speed;
+        this.y += ((Math.random() * 2) - 1) * speed;
     }
 }
 
