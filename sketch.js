c05c73cf62dd3deb5986c51ca669c2e12ddd0499
Atharva-Kapare <51484135+Atharva-Kapare@users.noreply.github.com>
2019-06-24 18:58:23 +1000

    Added DNA
    The DNA determines what the bug will do in it's lifetime, 
    Need to fix the problem where the bugs stop existing when they run out of DNA.

--- sketch.js
+++ sketch.js
@@ -2,10 +2,16 @@ let bugs = [];
 let size = 100; //the size of the population for each generation
 let currentPopulation = size; //amount of bugs alive at any given time
 let speed = 5; //the speed at which the bugs can move
+
+//start x and y initialise the start point of all the bugs
+//end x and y initialise the goal of the bugs
 let startX = 300;
 let startY = 450;
 let endX = 300;
 let endY = 150;
+
+//steps is the lifespan of all the bugs, this determines the amount of "steps" they can take before they dissappear
+let steps = 1000;
 
 function setup() {
 
@@ -34,14 +40,17 @@ function draw() {
     for (var i = 0; i < currentPopulation; i++) {
         bugs[i].show();
         bugs[i].move();
+        
 
         if (bugs[i].x > 600 || bugs[i].x < 0) {
             bugs.splice(i, 1);
             currentPopulation -= 1;
+            break;
         }
-        if (bugs[i].y > 600 || bugs[i].y < 0) {
+        else if (bugs[i].y > 600 || bugs[i].y < 0) {
             bugs.splice(i, 1);
             currentPopulation -= 1;
+            break;
         }
 
         //Collision system 
@@ -49,12 +58,13 @@ function draw() {
         if (hit) {
             bugs.splice(i, 1);
             currentPopulation -= 1;
+            break;
         }
     }
 
     //Drawing static obsticle
     rect(112, 285, 374, 30);
-
+    console.log(bugs.length);
 
 
 }
@@ -63,6 +73,12 @@ class Bug {
     constructor() {
         this.x = startX;
         this.y = startY;
+        this.counter = 0;
+        this.dna = [];
+        //initialising the DNA with random steps
+        for(var i = 0; i < steps; i++){
+            this.dna[i] = (Math.random() * 2) - 1;
+        }
 
         // this.x = Math.floor(Math.random() * 600);
         // this.y = Math.floor(Math.random() * 600);
@@ -74,8 +90,17 @@ class Bug {
     }
 
     move() {
-        this.x += ((Math.random() * 2) - 1) * speed;
-        this.y += ((Math.random() * 2) - 1) * speed;
+        
+        if(this.counter % 2 == 0){
+            this.x += this.dna[this.counter]*speed;
+        }
+        else if(this.counter % 2 == 1){
+            this.y += this.dna[this.counter]*speed;
+        }
+        this.counter++;
+
+        // this.x += ((Math.random() * 2) - 1) * speed;
+        // this.y += ((Math.random() * 2) - 1) * speed;
     }
 }
 
